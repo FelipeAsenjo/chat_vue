c0fdf117264691010c236b8490151a254f7fdc59
Felipe Asenjo <felipe.asenjo2@gmail.com>
2023-01-24 00:08:54 -0300

    [APP] UI for login page

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,7 +1,48 @@
 <template>
-    <h1>hola desde login</h1>
+    <section class="flex h-screen w-screen justify-center">
+        <div 
+            class="text-icon self-center w-5/6 p-4 
+                border border-icon rounded-lg md:w-80"
+        >
+            <form 
+                class="flex flex-col"
+                @submit.prevent="login"
+            >
+                <input 
+                    class="form-input my-3"
+                    type="text"
+                    placeholder="username"
+                />
+                <input 
+                    class="form-input my-3"
+                    type="password"
+                    placeholder="password"
+                />
+                <button 
+                    class="mx-auto my-3 px-4 py-1 border border-icon 
+                        rounded-lg hover:border-text hover:text-text"
+                >
+                    Login
+                </button>
+            </form>
+        </div>
+    </section>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
 
+        }
+    },
+    components: {
+
+    },
+    methods: {
+        login() {
+            console.log('trying to login')
+        }
+    }
+}
 </script>
