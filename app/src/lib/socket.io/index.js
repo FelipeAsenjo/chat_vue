817f3e82de27da099b5ffbd38fb9d13708555648
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-05-17 02:25:25 -0400

    Setup env variables

--- app/src/lib/socket.io/index.js
+++ app/src/lib/socket.io/index.js
@@ -1,5 +1,12 @@
 import { io } from 'socket.io-client'
+let url = import.meta.env.VITE_HOST_URL_DEV
+const prodEnv = import.meta.env.VITE_NODE_ENV_PROD
+const viteEnv = import.meta.env.MODE
 
-const socket = io('http://localhost:3000')
+if(viteEnv === prodEnv) {
+    url = import.meta.env.VITE_HOST_URL_PROD 
+}
+
+const socket = io(`${url}`)
 
 export default socket
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,9 +1,12 @@
+import { config } from 'dotenv'
 import devConfig from './dev.js'
 import prodConfig from './prod.js'
+const { NODE_ENV, VITE_NODE_ENV_PROD } = process.env
+config()
 
 let currentConfig = devConfig
 
-if(NODE_ENV === 'prod') {
+if(NODE_ENV === VITE_NODE_ENV_PROD) {
     currentConfig = prodConfig
 }
 // if(NODE_ENV === 'test') {
